432bd1d60ce435a8a36f67c136388eb856893a42
Tommy Bakken Mydland <tommy.bakken.mydland@innovasjonnorge.no>
2019-11-30 16:53:37 +0100

    added dependencies

---
+++ stockprediction-python.py
@@ -0,0 +1,12 @@
+# Install dependencies
+
+# pip install cython
+# pip install -U scikit-learn
+
+import quandl
+import numpy as np
+from sklearn.linear_model import LinearRegression
+from sklearn.svm import SVR
+from sklearn.model_selection import train_test_split
+
+
